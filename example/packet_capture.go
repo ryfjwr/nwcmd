d17e7aaa14b4416521a00fd9104f5a5745c99dc0
ryo fujiwara <fjwr0516@gmail.com>
2022-11-16 22:25:53 +0900

    update

--- example/packet_capture.go
+++ example/packet_capture.go
@@ -4,23 +4,28 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
 
 var (
-	pcapFile string = "http.cap"
-	handle   *pcap.Handle
-	err      error
+	handle *pcap.Handle
+	err    error
 )
 
 func main() {
 	// https://mrtc0.hateblo.jp/entry/2016/03/19/232252
 	// network interface を指定できる？
+
+	// https://qiita.com/kkyouhei/items/846e74c6a9653b069e5f
+	snapshotLen := 1024
+	timeOut := 600 * time.Second
+	promiscous := false
 	log.SetFlags(log.Llongfile)
 	//log.setFlags(log.Llongfile)
-	handle, err := pcap.OpenOffline(pcapFile)
+	handle, err := pcap.OpenLive("eth0", int32(snapshotLen), promiscous, timeOut)
 	if err != nil {
 		log.Fatal(err)
 	}
